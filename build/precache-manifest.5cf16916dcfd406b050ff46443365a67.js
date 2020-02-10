b0e935a39f2bce5610881ab5111732eb80cad89a
navdeepblock8 <navdeep.duvedi@block8.com>
2020-02-10 15:43:16 +0530

    commit

---
+++ build/precache-manifest.5cf16916dcfd406b050ff46443365a67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3673b88725746b906d956efb486a913b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8bb6a02a28da701f526",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0e4fd93db363ddc28623",
+    "url": "/static/js/2.08da2564.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.08da2564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8bb6a02a28da701f526",
+    "url": "/static/js/main.e6cb8eba.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
